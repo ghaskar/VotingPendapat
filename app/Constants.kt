f0675b32e1aadaceebcc2179eaf582af3a401414
ghaskar <77021993+ghaskar@users.noreply.github.com>
2025-11-12 01:48:48 +0700

    Create Constants.kt

---
+++ app/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.votinganon
+
+object Constants {
+    val ADMINS = listOf("+62822884187191", "+62823882059489")
+    const val ADMIN_PIN = "1234"
+    const val ADMIN_USER = "pasundan"
+    const val ADMIN_PASS = "pasundan"
+}
